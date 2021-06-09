bb3040304c1b3985e704f38e1aab2b0b1c706ddd
Arnaud Courtecuisse <arnaud.courtecuisse@webedia-group.com>
2021-06-09 18:16:15 +0200

    fix: diffSkills ignores live stages

--- test/diffSkills.js
+++ test/diffSkills.js
@@ -1,23 +1,22 @@
 'use strict';
 
-const expect = require('chai').expect;
+const { expect } = require('chai');
 const diffSkills = require('./../lib/diffSkills');
 
 describe('diffSkills()', () => {
-
   beforeEach(() => {
-    diffSkills.serverless = { service: { custom: { alexa: { skills: [] }}}};
+    diffSkills.serverless = { service: { custom: { alexa: { skills: [] } } } };
   });
 
   it('should return diff if it has the same ID', () => {
-    let localSkills = [{
+    const localSkills = [{
       id: 'foo',
-      manifest: { foo: 'bar' }
-    }]
-    let remoteSkills = [{
+      manifest: { foo: 'bar' },
+    }];
+    const remoteSkills = [{
       skillId: 'foo',
-      manifest: { foo: 'buz' }
-    }]
+      manifest: { foo: 'buz' },
+    }];
 
     diffSkills.serverless.service.custom.alexa.skills = localSkills;
     diffSkills.diffSkills(remoteSkills).then((ret) => {
@@ -26,18 +25,40 @@ describe('diffSkills()', () => {
   });
 
   it('should not return diff if it has not the same ID', () => {
-    let localSkills = [{
+    const localSkills = [{
       id: 'foo',
-      manifest: { foo: 'bar' }
-    }]
-    let remoteSkills = [{
+      manifest: { foo: 'bar' },
+    }];
+    const remoteSkills = [{
       skillId: 'bar',
-      manifest: { foo: 'buz' }
-    }]
+      manifest: { foo: 'buz' },
+    }];
 
     diffSkills.serverless.service.custom.alexa.skills = localSkills;
     diffSkills.diffSkills(remoteSkills).then((ret) => {
       expect(ret.length).to.equal(0);
+    });
+  });
+
+  it('should not return diff of live stages', () => {
+    const localSkills = [{
+      id: 'foo',
+      manifest: { foo: 'bar' },
+    }];
+    const remoteSkills = [{
+      skillId: 'foo',
+      stage: 'live',
+      manifest: { foo: 'buz' },
+    }, {
+      skillId: 'foo',
+      stage: 'development',
+      manifest: { foo: 'but' },
+    }];
+
+    diffSkills.serverless.service.custom.alexa.skills = localSkills;
+    diffSkills.diffSkills(remoteSkills).then((ret) => {
+      expect(ret.length).to.equal(1);
+      expect(ret[0].diff[0].lhs).to.equal('but');
     });
   });
 });
