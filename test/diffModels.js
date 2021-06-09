2c9f35588f1aa3da36053ebb834fd147aa99513c
Arnaud Courtecuisse <arnaud.courtecuisse@webedia-group.com>
2021-06-09 19:11:54 +0200

    fix: diffModels ignores remote version

--- test/diffModels.js
+++ test/diffModels.js
@@ -1,26 +1,25 @@
 'use strict';
 
-const expect = require('chai').expect;
+const { expect } = require('chai');
 const diffModels = require('./../lib/diffModels');
 
 describe('diffModels()', () => {
-
   beforeEach(() => {
-    diffModels.serverless = { service: { custom: { alexa: { skills: [] }}}};
+    diffModels.serverless = { service: { custom: { alexa: { skills: [] } } } };
   });
 
   it('should return diff if it has the same ID', () => {
-    let localSkills = [{
+    const localSkills = [{
       id: 'foo',
       models: {
-        'ja-JP': { foo: 'bar' }
-      }
-    }]
-    let remoteSkills = [[{
+        'ja-JP': { foo: 'bar' },
+      },
+    }];
+    const remoteSkills = [[{
       id: 'foo',
-      local: 'ja-JP',
-      model: { foo: 'buz' }
-    }]]
+      locale: 'ja-JP',
+      model: { foo: 'buz' },
+    }]];
 
     diffModels.serverless.service.custom.alexa.skills = localSkills;
     diffModels.diffModels(remoteSkills).then((ret) => {
@@ -29,21 +28,45 @@ describe('diffModels()', () => {
   });
 
   it('should not return diff if it has not the same ID', () => {
-    let localSkills = [{
+    const localSkills = [{
       id: 'foo',
       models: {
-        'ja-JP': { foo: 'bar' }
-      }
-    }]
-    let remoteSkills = [[{
+        'ja-JP': { foo: 'bar' },
+      },
+    }];
+    const remoteSkills = [[{
       id: 'bar',
-      local: 'ja-JP',
-      model: { foo: 'buz' }
-    }]]
+      locale: 'ja-JP',
+      model: { foo: 'buz' },
+    }]];
 
     diffModels.serverless.service.custom.alexa.skills = localSkills;
     diffModels.diffModels(remoteSkills).then((ret) => {
       expect(ret.pop().length).to.equal(0);
+    });
+  });
+
+  it('should ignore the remote model version', () => {
+    const localSkills = [{
+      id: 'foo',
+      models: {
+        'ja-JP': { foo: 'bar' },
+      },
+    }];
+    const remoteSkills = [[{
+      id: 'foo',
+      locale: 'ja-JP',
+      model: { foo: 'bar', version: '2' },
+    }], [{
+      id: 'foo',
+      locale: 'ja-JP',
+      model: { foo: 'buz', version: '1' },
+    }]];
+
+    diffModels.serverless.service.custom.alexa.skills = localSkills;
+    diffModels.diffModels(remoteSkills).then((ret) => {
+      expect(ret[0].pop().diff).to.equal(undefined);
+      expect(ret[1].pop().diff.length).to.equal(1);
     });
   });
 });
